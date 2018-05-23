34189c81190bb8df6ea372e4c58f5f661cbc563a
sunchangtan <sunchangtan@gmail.com>
2018-05-23 10:42:27 +0800

    URLNavigator

---
+++ URLNavigator/URLMatcher/URLMatcher.h
@@ -0,0 +1,19 @@
+//
+//  URLMatcher.h
+//  URLMatcher
+//
+//  Created by ChangtanSun on 2018/5/23.
+//  Copyright © 2018年 ChangtanSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for URLMatcher.
+FOUNDATION_EXPORT double URLMatcherVersionNumber;
+
+//! Project version string for URLMatcher.
+FOUNDATION_EXPORT const unsigned char URLMatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLMatcher/PublicHeader.h>
+
+
---
+++ URLNavigator/URLNavigator/URLNavigator.h
@@ -0,0 +1,19 @@
+//
+//  URLNavigator.h
+//  URLNavigator
+//
+//  Created by ChangtanSun on 2018/5/23.
+//  Copyright © 2018年 ChangtanSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for URLNavigator.
+FOUNDATION_EXPORT double URLNavigatorVersionNumber;
+
+//! Project version string for URLNavigator.
+FOUNDATION_EXPORT const unsigned char URLNavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLNavigator/PublicHeader.h>
+
+
